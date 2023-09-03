28e43d202f2ea08dbb7e409c5bae48150411886d
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-09-03 11:21:28 +0200

    update

--- src/index.js
+++ src/index.js
@@ -6,28 +6,53 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const refs = {
   formEL: document.querySelector('#search-form'),
   containerCards: document.querySelector('.gallery'),
+  btnLoadMor: document.querySelector('.load-more-hidden'),
 };
 
 let gallery = new SimpleLightbox('.gallery a');
+let currentPage = 1;
+let quantityImg = 0;
 
 refs.formEL.addEventListener('submit', onSubmitBtn);
 refs.containerCards.addEventListener('click', onClickCard);
+refs.btnLoadMor.addEventListener('click', onClickBtnLoadMor);
 
 function onSubmitBtn(evt) {
   evt.preventDefault();
   refs.containerCards.innerHTML = '';
-  const valueSearch = evt.target.elements.searchQuery.value;
+  refs.btnLoadMor.classList.replace('load-more', 'load-more-hidden');
 
-  if (valueSearch === ' ' || valueSearch === '') {
+  const valueSearch = evt.target.elements.searchQuery.value.trim();
+  localStorage.setItem('input-value', valueSearch);
+  let currentPage = 1;
+
+  if (!valueSearch) {
     return Notify.failure('Enter your search details.');
   }
-  serviceSearchImg(valueSearch)
+  serviceSearchImg(currentPage, valueSearch)
     .then(data => {
+      quantityImg += data.hits.length;
       createCards(data.hits);
       refs.containerCards.insertAdjacentHTML(
         'beforeend',
         createCards(data.hits)
       );
+      if (data.totalHits !== 0) {
+        Notify.info(`"Hooray! We found ${data.totalHits} images."`);
+      }
+
+      if (data.totalHits > quantityImg) {
+        refs.btnLoadMor.classList.replace('load-more-hidden', 'load-more');
+      }
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
     })
     .catch(error => {
       Notify.failure(error.message);
@@ -42,8 +67,30 @@ function onClickCard(evt) {
   gallery.next();
 }
 
+function onClickBtnLoadMor() {
+  const inputValue = localStorage.getItem('input-value');
+  currentPage += 1;
+  serviceSearchImg(currentPage, inputValue)
+    .then(data => {
+      quantityImg += data.hits.length;
+      createCards(data.hits);
+      refs.containerCards.insertAdjacentHTML(
+        'beforeend',
+        createCards(data.hits)
+      );
+      if (data.totalHits <= quantityImg) {
+        refs.btnLoadMor.classList.replace('load-more', 'load-more-hidden');
+      }
+    })
+    .catch(error => {
+      Notify.failure(error.message);
+    })
+    .finally(() => {
+      gallery.refresh();
+    });
+}
+
 function createCards(arr) {
-  console.log(arr);
   return arr
     .map(
       ({
@@ -55,25 +102,25 @@ function createCards(arr) {
         comments,
         downloads,
       }) => {
-        return `<div class ="photo-card">
+        return `<div class ="photo-card"> 
        <a class="gallery-link" href="${largeImageURL}"> 
-       <img src="${webformatURL}" alt="${tags}" width = "" height ="" loading="lazy" />
+       <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+       <div class="info">
+       <p class="info-item">
+         <b>Likes <span class= "item-text">${likes}</span></b>
+       </p>
+       <p class="info-item">
+         <b>Views <span class= "item-text">${views}</span></b>
+       </p>
+       <p class="info-item">
+         <b>Comments <span class= "item-text">${comments}</span></b>
+       </p>
+       <p class="info-item">
+         <b>Downloads <span class= "item-text">${downloads}</span></b>
+       </p>
+     </div>
        </a>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes <span class= "item-text">${likes}</span></b>
-          </p>
-          <p class="info-item">
-            <b>Views <span class= "item-text">${views}</span></b>
-          </p>
-          <p class="info-item">
-            <b>Comments <span class= "item-text">${comments}</span></b>
-          </p>
-          <p class="info-item">
-            <b>Downloads <span class= "item-text">${downloads}</span></b>
-          </p>
-        </div>
-        </div>`;
+    </div>`;
       }
     )
     .join('');
--- src/js/img-api.js
+++ src/js/img-api.js
@@ -3,13 +3,17 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-async function serviceSearchImg(valueSearch) {
+async function serviceSearchImg(currentPage, valueSearch) {
   const params = new URLSearchParams({
     key: '39204172-43f1bb7905ec0f3758e1627d0',
-    q: `${valueSearch}`,
+    q: valueSearch,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    min_width: '360',
+    min_height: '240',
+    per_page: '40',
+    page: currentPage,
   });
   try {
     const response = await axios.get(`?${params}`);
@@ -18,6 +22,7 @@ async function serviceSearchImg(valueSearch) {
         'Sorry, there are no images matching your search query. Please try again.'
       );
     }
+    console.log(response);
     return response.data;
   } catch (error) {
     Notify.failure(error.message);
